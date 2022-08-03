e2ae32b5225da43f18b93f0a0bcb813a70eb028e
Camilo Arriagada Vallejos <95378920+c4miloarriagada@users.noreply.github.com>
2022-08-03 16:39:04 -0400

    folders

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,27 @@
+
+
+
+export const todoReducer = (state = [], action)=>{
+
+    switch ( action.type ) {
+         case 'ADD':
+             return [...state, action.payload];
+         case 'DELETE':
+            return state.filter(todo => todo.id !== action.payload); 
+         case 'COMPLETE': 
+            return state.map(todo =>{
+                if(todo.id === action.payload){
+                    return{
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+                return todo;
+            });    
+        
+        default:
+            return state;
+    }
+
+
+}
